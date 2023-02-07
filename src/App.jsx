b92bc33eaac2116b4483895d8a1a4898b74cc5a2
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-02-07 15:05:15 +0000

    Styling and spacing

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
     
     <div className="App">
       <h1 className='title'>Ticket Tracker</h1>
+      <div className="search-fields">
+        <select>
+          <option value="0">Search by role:</option>
+          <option value="1">Junior Software Develope</option>
+          <option value="2">Software Developer</option>
+          <option value="3">Senior Software Developer</option>
+          <option value="4">Project Manager</option>
+          <option value="5">Tester</option>
+          <option value="6">Hard Man</option>
+        </select>
+        Search by name:<input type="text" />
+      </div>
+      
       <CardContainer/>
     </div>
   );
